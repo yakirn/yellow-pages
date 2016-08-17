51b6685aaf13a4f3cc938e4dd2259edce16cb6a9
yakirn <yak.irna@gmail.com>
2016-08-17 12:07:10 +0300

    Implemented search

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,44 +1,35 @@
 class PeopleController < ApplicationController
-  before_action :set_person, only: [:show, :update, :destroy]
-
-  # GET /people
-  def index
-    @people = Person.all
-
-    render json: @people
-  end
+  before_action :set_person, only: [:show]
 
   # GET /people/1
   def show
     render json: @person
   end
 
-  # POST /people
-  def create
-    @person = Person.new(person_params)
-
-    if @person.save
-      render json: @person, status: :created, location: @person
-    else
-      render json: @person.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /people/1
-  def update
-    if @person.update(person_params)
-      render json: @person
-    else
-      render json: @person.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /people/1
-  def destroy
-    @person.destroy
+  def search
+      query = Person.order(name: :asc)
+                    .select(search_select_fields)
+      query = query.where("name LIKE ?", "%#{params[:name]}%") if params[:name]
+      if params[:age]
+          age = Integer(params[:age], 10)
+          query = query.where("age = ?", age)
+      end
+      query = query.where("phone LIKE ?", "%#{params[:phone]}%") if params[:phone]
+      #TODO: handle a case where no name, age, or phone were given
+      res = query.to_a
+      render json: {total: res.length, people: res.first(20)}
   end
 
   private
+
+    def search_select_fields
+        "id, name, phone, picture, #{calc_age} AS age, printf('%s. %s, %s.', street, country, city) as address"
+    end
+
+    def calc_age
+        "(strftime('%Y', 'now') - strftime('%Y', birthday)) - (strftime('%m-%d', 'now') < strftime('%m-%d', birthday))"
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_person
       @person = Person.find(params[:id])
